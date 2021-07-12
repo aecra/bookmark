0aa9b653bf76a0ba9a512b81231d38ebd60b2fb4
aecra <2572607031@qq.com>
2021-07-13 00:16:44 +0800

    使用eslint、airbnb

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -1,13 +1,13 @@
 // 云函数入口文件
-const cloud = require('wx-server-sdk')
+const cloud = require('wx-server-sdk');
 
-cloud.init()
+cloud.init();
 
 // 云函数入口函数
-exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
+exports.main = async () => {
+  const wxContext = cloud.getWXContext();
 
   return {
     openid: wxContext.OPENID,
-  }
-}
+  };
+};
--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,4 +1,4 @@
-//app.js
+// app.js
 App({
   globalData: {
     openid: '',
@@ -7,36 +7,38 @@ App({
       webdavurl: '',
       username: '',
       password: '',
-      path: ''
-    }
+      path: '',
+    },
   },
-  onLaunch: function () {
+  onLaunch() {
     if (!wx.cloud) {
-      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力');
     } else {
       wx.cloud.init({
         env: 'aecra-bookmark-4gqya2jb3376f9bf',
         traceUser: true,
-      })
+      });
     }
 
     wx.cloud.callFunction({
-      name: 'login'
-    }).then(data => {
+      name: 'login',
+    }).then((data) => {
       this.globalData.openid = data.result.openid;
       this.getConfig();
     });
   },
   getConfig() {
     const db = wx.cloud.database();
-    db.collection('config').where({ _openid: this.globalData.openid }).get({
-      success: res => {
-        if (res.data.length != 0) {
+    db.collection('config').where({
+      _openid: this.globalData.openid,
+    }).get({
+      success: (res) => {
+        if (res.data.length !== 0) {
           this.globalData.hasConfig = true;
           this.globalData.config = res.data[0].config;
-        };
-      }
-    })
+        }
+      },
+    });
   },
   setConfig(config) {
     this.globalData.hasConfig = true;
@@ -44,19 +46,19 @@ App({
     const db = wx.cloud.database();
     db.collection('config').add({
       data: {
-        config: this.globalData.config
-      }
-    })
+        config: this.globalData.config,
+      },
+    });
   },
   updateConfig(config) {
     this.globalData.config = config;
     const db = wx.cloud.database();
     db.collection('config').where({
-      _openid: this.globalData.openid
+      _openid: this.globalData.openid,
     }).update({
       data: {
-        config: this.globalData.config
-      }
-    })
-  }
-})
+        config: this.globalData.config,
+      },
+    });
+  },
+});
