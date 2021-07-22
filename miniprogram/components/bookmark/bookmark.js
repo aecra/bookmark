d1ad4f66b55cb394f165940dfc3cc3e5d48e3aba
aecra <2572607031@qq.com>
2021-07-22 20:21:55 +0800

    实现书签的单层目录展示

---
+++ miniprogram/components/bookmark/bookmark.js
@@ -0,0 +1,23 @@
+// components/bookmark/bookmark.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    content: Object,
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  },
+});
